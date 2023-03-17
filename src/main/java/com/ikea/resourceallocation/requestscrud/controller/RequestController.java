8b1d7ab55ba815c6cf438fbfcd0240e765d9c922
Shubham Jangale <shubham-rajendra.jangale@capgemini.com>
2023-03-17 21:46:44 +0530

    added cors in controller

--- src/main/java/com/ikea/resourceallocation/requestscrud/controller/RequestController.java
+++ src/main/java/com/ikea/resourceallocation/requestscrud/controller/RequestController.java
@@ -12,6 +12,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -31,7 +32,7 @@ import com.ikea.resourceallocation.requestscrud.service.RequestService;
 import com.ikea.resourceallocation.requestscrud.service.impl.ExcelExporter;
 import com.ikea.resourceallocation.requestscrud.service.impl.PDFExporter;
 
-//@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 @RequestMapping("/api/req")
 public class RequestController {
